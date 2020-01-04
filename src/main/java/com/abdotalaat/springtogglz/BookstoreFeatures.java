64768d141e6b7bffa6976a8193696690b307f18a
abdotalaat <abdotalaatfcih@gmail.com>
2020-01-04 22:33:40 +0400

    init commint

---
+++ src/main/java/com/abdotalaat/springtogglz/BookstoreFeatures.java
@@ -0,0 +1,15 @@
+package com.abdotalaat.springtogglz;
+
+import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
+import org.togglz.core.annotation.Label;
+
+public enum BookstoreFeatures implements Feature {
+
+    @EnabledByDefault
+    @Label("Provide extended information about a book")
+    EXTENDED_INFORMATION,
+
+    @Label("Enable public prices endpoint for books")
+    PUBLIC_PRICES
+}
---
+++ src/test/java/com/abdotalaat/springtogglz/SpringTogglzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdotalaat.springtogglz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTogglzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
